26f3db73f473e2c0b9edec702961df1f40e701b5
roni ramon <roniramon260@gmail.com>
2018-03-30 17:54:48 -0700

    session reducer

--- benchBnb/frontend/actions/session_actions.js
+++ benchBnb/frontend/actions/session_actions.js
@@ -1,19 +1,33 @@
 import * as APIUtil from '../utils/session_api_util';
 
 export const RECEIVE_CURRENT_USER = 'RECEIVE_CURRENT_USER';
-export const LOGOUT_CURRENT_USER = 'LOGOUT_CURRENT_USER';
+export const RECEIVE_SESSION_ERRORS = 'RECEIVE_SESSION_ERRORS';
 
 export const receiveCurrentUser = (currentUser) => ({
   type: RECEIVE_CURRENT_USER,
   currentUser,
 });
 
+export const receiveErrors = (errors) => ({
+  type: RECEIVE_SESSION_ERRORS,
+  errors,
+});
+
 export const login = (currentUser) => dispatch => {
   APIUtil.login(currentUser)
-  .then( (user) => dispatch(receiveCurrentUser(user)));
+  .then( (user) => dispatch(receiveCurrentUser(user))
+    ,(err) => dispatch(receiveErrors(err.responseJSON))
+  );
 };
 
 export const signup = (currentUser) => dispatch => {
   APIUtil.signup(currentUser)
-  .then((user)=> dispatch(receiveCurrentUser(user)));
+  .then((user)=> dispatch(receiveCurrentUser(user))
+    ,(err) => dispatch(receiveErrors(err.responseJSON))
+);
+};
+
+export const logout = () => dispatch => {
+  APIUtil.logout()
+  .then((user)=>dispatch(receiveCurrentUser(null)));
 };
